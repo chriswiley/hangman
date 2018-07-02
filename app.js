1e022c8c0f61382e09509bd63996444d78279f6a
Chris Wiley <chris@genesis-webdesign.com>
2018-07-02 04:57:01 -0400

    updated more of the code

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-// Primative value: string, number, boolean, null, undefined
-
-// Object: myObject --> Object.prototype --> null
-// Array: myArray --> Array.prototype --> Object.prototype --> null
-// Function: myFunction --> function.prototype --> Object.prototype --> null
-// String: myString --> String.prototype --> Object.prototype --> null
-// Number: myNumber --> Number.prototype --> Object.prototype --> null
-// Boolean: myBoolean --> Boolean.prototype --> Object.prototype --> null
-
-// const product = 'Computer'
-// console.log(product)
-
-// const otherProduct = new String('Phone')
-// console.log(otherProduct)
-
-
-
 const first = new Hangman('Cat', 3)
 first.getPuzzle()
 window.addEventListener('keypress', function (e) {
--- hangman.js
+++ hangman.js
@@ -6,8 +6,8 @@ const wrongGuess = document.querySelector('#wrong-guess')
 
 const Hangman = function (wordToGuess, numberOfGuesses) {
   this.wordToGuess = wordToGuess.toLowerCase().split('')
-  // this.wordToGuess = Array.of(wordToGuess.toLowerCase())
   this.numberOfGuesses = numberOfGuesses
+  this.status = 'playing'
   this.guessedLetters = []
 }
 
@@ -26,6 +26,7 @@ Hangman.prototype.getPuzzle = function () {
   })
   build += `</h2>`
   guessesLeft.innerHTML = `<p style="color:${this.numberOfGuesses > 1 ? 'blue' : 'red'}; ${this.numberOfGuesses <= 1 ? 'font-size:2em' : ''}">You have ${this.numberOfGuesses} guess${this.numberOfGuesses === 1 ? '' : 'es'} remaining</p>`
+  console.log(this.status)
   return puzzleHTML.innerHTML = build
 }
 
