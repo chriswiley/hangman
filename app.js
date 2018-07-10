b9c9dc62c898e8f5eb12e09fa19a3dd5c9d3bab1
Chris Wiley <chris@genesis-webdesign.com>
2018-07-10 03:27:49 -0400

    Setting up api request

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
+// HTTP (Hypertext Transfer Protocol)
+// Request - What do we want to do
+// Response - What was actually done.
+
 const first = new Hangman('Car parts', 3)
 
 first.getPuzzle()
 
-const _keyPress = function (e) {
+const _keyPress = (e) => {
   const guess = String.fromCharCode(e.charCode)
   first.makeGuess(guess)
 }
 
 window.addEventListener('keypress', _keyPress)
+
+
+// Making an HTTP request
+const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechange', (e) => {
+  if (e.target.readyState === 4 && e.target.status === 200) {
+    const data = JSON.parse(e.target.responseText)
+    console.log(data)
+  } else if (e.target.readyState === 4) {
+    console.log('an error has taken place')
+  }
+})
+
+request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
+request.send()
