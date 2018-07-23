f956b5aea5530f4dc06d92ae495d76d32607ccdd
Chris Wiley <chris@genesis-webdesign.com>
2018-07-22 21:58:42 -0400

    Fun with Callbacks

--- requests.js
+++ requests.js
@@ -1,5 +1,5 @@
 
-const getPuzzle = (callback) => {
+const getPuzzle = (wordCount, callback) => {
 
   const request = new XMLHttpRequest()
 
@@ -12,7 +12,23 @@ const getPuzzle = (callback) => {
     }
   })
 
-  request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
+  request.open('GET', `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
   request.send()
 }
 
+const getCountry = (countryCode, callback) => {
+  const countryRequest = new XMLHttpRequest()
+
+  countryRequest.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+      const data = JSON.parse(e.target.responseText)
+      const country = data.find((country) => country.alpha2Code === countryCode.toUpperCase())
+      callback(undefined, country)
+    } else if (e.target.readyState === 4) {
+      callback("Dude! You don't know your two digit code?")
+    }
+  })
+
+  countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
+  countryRequest.send()
+}
